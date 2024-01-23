af9f29605a90e3ed6b778f442f5e109e4a09a688
desert71 <amkazennov99@gmail.com>
2024-01-23 08:48:53 +0300

    add .gitignore + .venv

--- simple_API.py
+++ simple_API.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+import uvicorn
+from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi import FastAPI, Request
+from envparse import Env
 from fastapi.routing import APIRouter, APIRoute
+
+env = Env()
+MONGODB_URL = env.str("MONGODB_URL", default="mongodb://localhost:27017/test_database")
 
 app = FastAPI()
 
@@ -41,9 +47,27 @@ async def mainpage() -> str:
 
             }
 
+async def create_record(request: Request) -> dict:
+    mongo_client: AsyncIOMotorClient = request.app.state.mongo_client["test_database"]
+    await mongo_client.records.insert_one({"sample": "record!!!!"})
+    return {"Success": True}
+
+async def get_record(rrequest: Request) -> list:
+    mongo_client: AsyncIOMotorClient = rrequest.app.state.mongo_client["test_database"]
+    cursor = mongo_client.records.find({})
+    res = []
+    for document in await cursor.to_list(length=100):
+        document["_id"] = str(document["_id"])
+        res.append(document)
+    return res
+
 routes = [
     APIRoute(path="/ping", endpoint=ping, methods=["GET"]),
-    APIRoute(path="/", endpoint=mainpage, methods=["GET"], response_model=dict),
+    APIRoute(path="/", endpoint=mainpage, methods=["GET"]),
+    APIRoute(path="/create_record", endpoint=create_record, methods=["POST"]),
+    APIRoute(path="/get_record", endpoint=get_record, methods=["GET"]),
 ]
-
+client = AsyncIOMotorClient(MONGODB_URL)
+app = FastAPI()
+app.state.mongo_client = client
 app.include_router(APIRouter(routes=routes))
